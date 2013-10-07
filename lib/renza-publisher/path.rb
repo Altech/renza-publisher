f66656f6d678977c45608a25f837a2d3e6e23202
Altech <takeno.sh@gmail.com>
2013-10-07 20:37:35 +0900

    予定していたアップロードツールの機能を一通り実装
    動画とサムネイルのアップロードする。
    番号またはそのレンジで特定の動画を指定できる。
    一行目に表示される説明文を指定できる。

--- lib/renza-publisher/path.rb
+++ lib/renza-publisher/path.rb
@@ -9,12 +9,12 @@ class RenzaPublisher
     THUMBNAIL_FORMAT = "vol%03d_thumb.jpg"
     VIDEO_FORMAT_BASE = "vol%03d"
     
-    attr_accessor :working_dir, :output_dir
+    attr_accessor :working_dir, :data_dir
     attr_accessor :video_extension
 
-    def initialize(working_dir, output_dir, video_extension)
-      @working_dir, @output_dir = Pathname.new(working_dir), Pathname.new(output_dir)
-      [@working_dir, @output_dir].each do |dir| dir.mkpath end
+    def initialize(working_dir, data_dir, video_extension)
+      @working_dir, @data_dir = Pathname.new(working_dir), Pathname.new(data_dir)
+      [@working_dir, @data_dir].each do |dir| dir.mkpath end
       @video_extension = video_extension
     end
     
@@ -23,19 +23,19 @@ class RenzaPublisher
     end
     
     def video_file(i)
-      sprintf((@output_dir + (VIDEO_FORMAT_BASE+@video_extension)).to_s, i)
+      sprintf((@data_dir + (VIDEO_FORMAT_BASE+@video_extension)).to_s, i)
     end
     
     def thumbnail_file(i)
-      sprintf((@output_dir + THUMBNAIL_FORMAT).to_s, i)
+      sprintf((@data_dir + THUMBNAIL_FORMAT).to_s, i)
     end
 
     def time_table_file(disk_number)
-      sprintf((@output_dir + TIME_TABLE_FORMAT).to_s, disk_number)
+      sprintf((@data_dir + TIME_TABLE_FORMAT).to_s, disk_number)
     end
 
     def metadata_file
-      (@output_dir + METADATA).to_s
+      (@data_dir + METADATA).to_s
     end
 
     def masking_file_for_beggining
