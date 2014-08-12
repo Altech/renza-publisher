4f25d795f3b0c857f73f8216c53e19704a01b352
Altech <takeno.sh@gmail.com>
2014-08-12 15:09:28 +0900

    二画面合成動画のためにメタデータの形式などを変更

--- lib/renza-publisher/path.rb
+++ lib/renza-publisher/path.rb
@@ -2,24 +2,25 @@ class RenzaPublisher
   class Path
     require 'pathname'
 
-    SAMPLE_DIR_FORMAT = "samples_disk%d"
+    SAMPLE_DIR_FORMAT = "samples_%s_disk%d"
     SAMPLE_BASENAMES = %w[beginning ending_win ending_lose]
     MASKING_FILE_FORMAT = "mask%d.jpg"
-    TIME_TABLE_FORMAT = "time_table_disk%d.json"
+    TIME_TABLE_FORMAT = "time_table_%s_disk%d.json"
     METADATA = "metadata.json"
     IMAGE_FORMAT = "image-%04d.jpg"
-    THUMBNAIL_FORMAT = "vol%03d_thumb.jpg"
-    VIDEO_FORMAT_BASE = "vol%03d"
+    THUMBNAIL_FORMAT = "vol%03d__.jpg"
+    VIDEO_FORMAT_BASE = "vol%03d_%s"
     
     attr_accessor :working_dir, :data_dir
     attr_accessor :video_extension
     attr_accessor :disk_number
 
-    def initialize(working_dir, data_dir, video_extension, disk_number)
+    def initialize(working_dir, data_dir, video_extension, disk_number, seat)
       @working_dir, @data_dir = Pathname.new(working_dir), Pathname.new(data_dir)
       [@working_dir, @data_dir].each do |dir| dir.mkpath end
       @video_extension = video_extension
       @disk_number = disk_number
+      @seat = seat
     end
     
     def image_file(i)
@@ -27,11 +28,12 @@ class RenzaPublisher
     end
     
     def video_file(i)
-      sprintf((@data_dir + (VIDEO_FORMAT_BASE+@video_extension)).to_s, i)
+      sprintf((@data_dir + (VIDEO_FORMAT_BASE+@video_extension)).to_s, i, @seat)
     end
     
     def thumbnail_file(i, suffix = nil)
-      p = sprintf((@data_dir + THUMBNAIL_FORMAT).to_s, i)
+      dir = suffix.nil? ? @data_dir : @data_dir + 'thumbnails'
+      p = sprintf((dir + THUMBNAIL_FORMAT).to_s, i)
       if suffix.nil? 
         p
       else
@@ -39,8 +41,8 @@ class RenzaPublisher
       end
     end
 
-    def time_table_file(disk_number = @disk_number)
-      sprintf((@data_dir + TIME_TABLE_FORMAT).to_s, disk_number)
+    def time_table_file(seat = @seat, disk_number = @disk_number)
+      sprintf((@data_dir + TIME_TABLE_FORMAT).to_s, seat, disk_number)
     end
 
     def metadata_file
@@ -56,7 +58,7 @@ class RenzaPublisher
     end
 
     def sample_dir
-      sprintf((@data_dir +  SAMPLE_DIR_FORMAT).to_s, @disk_number)
+      sprintf((@working_dir +  SAMPLE_DIR_FORMAT).to_s, @seat, @disk_number)
     end
 
     def sample_dir_include_all_samples?
@@ -72,7 +74,6 @@ class RenzaPublisher
     def sample_files
       SAMPLE_BASENAMES.map{|s| sample_dir + '/' + s + '.jpg'}
     end
-
 
     def add_suffix(path, suffix)
       path = Pathname.new(path)
